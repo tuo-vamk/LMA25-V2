ad344828b02c92640ef36cb7a4981c6792d7fd17
Matti <git@jakomail.com>
2025-11-27 09:40:33 +0200

    Added persistence project for database connection

--- LMA25-V2/MauiProgram.cs
+++ LMA25-V2/MauiProgram.cs
@@ -1,9 +1,11 @@
 ﻿using LMA25_V2.Interfaces;
 using LMA25_V2.Pages;
+using LMA25_V2.Persistence;
 using LMA25_V2.Services;
 using LMA25_V2.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
 
 
 namespace LMA25_V2
@@ -27,6 +29,13 @@ namespace LMA25_V2
 
             // Register HttpClient
             builder.Services.AddHttpClient();
+
+            // Register DbContext with SQLite
+            // Run in app-project folder:
+            // dotnet ef migrations add InitialCreate --project ..\Persistence --startup-project .
+            // dotnet ef database update --project ..\Persistence --startup-project .
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite($"Filename={Path.Combine(FileSystem.AppDataDirectory, "app.db")}"));
 
             // Register ViewModels
             //MainPage and MainViewModel → Singleton because they are the app’s root.
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using LMA25_V2.Models;
+
+namespace LMA25_V2.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
+
+
+
